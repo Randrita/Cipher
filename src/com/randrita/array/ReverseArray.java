3c31cb8cfbda3044314c4907e34c94292a8c9d6b
Randrita Sarkar <60352282+Randrita@users.noreply.github.com>
2021-12-08 17:29:13 +0530

    reverse array problem added

---
+++ src/com/randrita/array/ReverseArray.java
@@ -0,0 +1,29 @@
+package com.randrita.array;
+import java.util.Scanner;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //declare the array
+        int[] arr = new int[5];
+
+        //take the array input
+        for(int i=0;i<arr.length;i++){
+            arr[i]=input.nextInt();
+        }
+
+        //displaying the input array
+        System.out.println("The original array:");
+        for(int num:arr){
+            System.out.print(num+" ");
+        }
+
+        //reversing the array
+        System.out.println("\nThe reverse array:");
+        for(int i = arr.length-1;i>=0;i-- ){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
+}
