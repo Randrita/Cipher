2a7d2d0de8a37010a924530418f913df06f6cd51
Randrita Sarkar <60352282+Randrita@users.noreply.github.com>
2022-01-07 18:01:07 +0530

    Singly Linked List Solution added

---
+++ src/com/randrita/LL/Main.java
@@ -0,0 +1,13 @@
+package com.randrita.LL;
+
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+
+        list.insertAtFirst(10);
+        list.insertAtFirst(20);
+        list.insertAtFirst(30);
+
+        list.display();
+    }
+}
