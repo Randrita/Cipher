29d9d83adaafa79048bf3787dc9aaf01e2a646db
Randrita Sarkar <60352282+Randrita@users.noreply.github.com>
2022-01-09 01:55:17 +0530

    recursion- fibonacci

---
+++ src/com/randrita/Recursion/Fibonacci.java
@@ -0,0 +1,19 @@
+package com.randrita.Recursion;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println(fibo(6));
+    }
+
+    public static int fibo(int n){
+        //base condition
+        if(n<2){
+            return n;
+        }
+
+        //recurrence relation
+        return fibo(n-1)+fibo(n-2);
+    }
+
+}
+
